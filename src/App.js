4ed53c8f0127380931c516b65c0be8691bf3869d
Ernesto Cardenas <ernesto.cardenas@unosqueare.com>
2024-05-24 13:01:13 -0600

    split the App into it's own file and imported

---
+++ src/App.js
@@ -0,0 +1,29 @@
+ const Pet = (prop) => {
+        return React.createElement("div", {}, [
+          React.createElement("h1", {}, prop.name),
+          React.createElement("h2", {}, prop.animal),
+          React.createElement("h2", {}, prop.breed),
+        ]);
+      };
+
+      const App = () => {
+        return React.createElement(
+          "div",
+          {},
+          React.createElement("h1", {}, "Adopt Me!"),
+          React.createElement(Pet, {
+            name: "Canelito",
+            animal: "Cat",
+            breed: "furCat",
+          }),
+          React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+          }),
+        );
+      };
+
+      const container = document.getElementById("root");
+      const root = ReactDOM.createRoot(container);
+      root.render(React.createElement(App));
